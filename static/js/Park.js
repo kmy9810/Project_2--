1e050c5e735814ac32ce7a33e274079467dec5f2
miyeong <kmy9810@naver.com>
2023-03-16 20:48:09 +0900

    modify all

--- static/js/Park.js
+++ static/js/Park.js
@@ -277,13 +277,3 @@ function openTab(evt, tab) {
   evt.currentTarget.className += " active";
 }
 
-// Horizontal scroll for the tabs on mousewheel. If tabs are longer than the
-// content section, there's a scroll bar but it's hidden to retain the design.
-if (window.innerWidth > 800) {
-  const scrollContainer = document.querySelector(".tabs");
-
-  scrollContainer.addEventListener("wheel", (evt) => {
-      evt.preventDefault();
-      scrollContainer.scrollLeft += evt.deltaY;
-  });
-}
